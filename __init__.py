5295fb22c5f60ac6db498abb613979c3b1ef14c1
Ian Bakst <ianbakst@Ians-MacBook-Pro.local>
2019-09-30 21:07:43 -0400

    Actually deleted files

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-from flaskapp import views
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
--- run.py
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/python
-from flaskapp import app
-app.config['TEMPLATES_AUTO_RELOAD']=True
-app.run(debug = True,host= '0.0.0.0')
